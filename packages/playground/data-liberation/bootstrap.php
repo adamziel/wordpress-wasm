98244cc1e9c9df53b4c9dd02faa1327fddca9fd1
Adam Zieliński <adam@adamziel.com>
2024-10-29 15:48:59 +0100

    [Data Liberation] Merge both XML processors into a single WP_XML_Processor
    Description TBD

--- packages/playground/data-liberation/bootstrap.php
+++ packages/playground/data-liberation/bootstrap.php
@@ -30,7 +30,6 @@ require_once __DIR__ . '/src/WP_URL_In_Text_Processor.php';
 require_once __DIR__ . '/src/WP_URL.php';
 
 require_once __DIR__ . '/src/xml-api/WP_XML_Decoder.php';
-require_once __DIR__ . '/src/xml-api/WP_XML_Tag_Processor.php';
 require_once __DIR__ . '/src/xml-api/WP_XML_Processor.php';
 require_once __DIR__ . '/src/WP_WXR_URL_Rewrite_Processor.php';
 
