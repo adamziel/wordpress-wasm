ce299fbb27355dd1d5dbcb04bdcbd022705ffa99
Paulo Pinto <paulo.pinto@automattic.com>
2024-12-06 11:58:41 +0000

    Implement script to enable or disable local registry

--- tools/scripts/local-registry.mjs
+++ tools/scripts/local-registry.mjs
@@ -1,22 +1,49 @@
 #!/usr/bin/env node
 
+import { execSync, spawnSync } from 'child_process';
+
+const localRegistryUrl = 'http://localhost:4873/';
+
 const [, , command] = process.argv;
-
-switch (command) {
-	case 'enable':
-		enable();
-		break;
-	case 'disable':
-		disable();
-		break;
-	default:
-		console.error(`unknown command: ${command}`);
+if (!['enable', 'disable'].includes(command)) {
+	console.error(`unknown command: ${command}`);
 }
 
-function enable() {
-	console.log(`enable`);
+const result = spawnSync('npm', ['config', 'get', 'registry'], {});
+const currentRegistry = result.stdout.toString().trim();
+
+if (command === 'enable' && currentRegistry === localRegistryUrl) {
+	console.log(`Local registry is already enabled: ${localRegistryUrl}`);
+	process.exit(0);
+} else if (command === 'disable' && currentRegistry !== localRegistryUrl) {
+	console.log('Local registry is already disabled');
+	console.log(`Active registry is ${currentRegistry}`);
+	process.exit(0);
 }
 
-function disable() {
-	console.log(`disable`);
+// Modifies .npmrc at the repo root.
+if (command === 'enable') {
+	console.log(`Setting registry to ${localRegistryUrl}`);
+	spawnSync(
+		'npm',
+		[
+			'config',
+			'set',
+			'registry',
+			localRegistryUrl,
+			'--location',
+			'project',
+		],
+		{}
+	);
+	console.log(
+		'Make sure to start the local registry with: npm run local-registry:start'
+	);
+} else if (command === 'disable') {
+	console.log(`Disabling local registry`);
+	spawnSync(
+		'npm',
+		['config', 'delete', 'registry', '--location', 'project'],
+		{}
+	);
 }
