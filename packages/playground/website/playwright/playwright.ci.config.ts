889267210d5bf00bee93601a06c0f1d75c3a4201
Adam Zieliński <adam@adamziel.com>
2024-12-10 16:05:03 +0100

    Use preview-server instead of preview:server job name

--- packages/playground/website/playwright/playwright.ci.config.ts
+++ packages/playground/website/playwright/playwright.ci.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
 	webServer: {
 		command: 'npx nx run playground-website:preview:ci',
 		url: 'http://127.0.0.1/',
-		timeout: 120000,
 		reuseExistingServer: false,
 	},
 });
