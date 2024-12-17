1c66dd0f1efe09fc1ebd9b83c5054e20ad79607b
Adam Zieliński <adam@adamziel.com>
2024-12-17 13:36:04 +0100

    [Data Liberation] Add HTML to Blocks converter
    Adds a basic WP_HTML_To_Blocks class that accepts HTML and outputs block markup.
    
    It only considers the markup and won't consider any visual changes introduced via CSS or JavaScript.
    
    A part of #1894
    
     ## Example
    
    ```html
    $html = <<<HTML
    <meta name="post_title" content="My first post">
    <p>Hello <b>world</b>!</p>
    HTML;
    
    $converter = new WP_HTML_To_Blocks( $html );
    $converter->convert();
    
    var_dump( $converter->get_all_metadata() );
    /*
     * array( 'post_title' => array( 'My first post' ) )
     */
    
    var_dump( $converter->get_block_markup() );
    /*
     * <!-- wp:paragraph -->
     * <p>Hello <b>world</b>!</p>
     * <!-- /wp:paragraph -->
     */
    ```
    
     ## Testing instructions
    
    This PR mostly adds new code. Just confirm the unit tests pass in CI.

---
+++ packages/playground/data-liberation/src/import/WP_Import_Utils.php
@@ -0,0 +1,96 @@
+<?php
+
+class WP_Import_Utils {
+
+	public static function block_opener( $block_name, $attrs = array() ) {
+		$encoded_attrs = json_encode( $attrs );
+		if ( $attrs && $encoded_attrs !== '{}' ) {
+			$encoded_attrs .= ' ';
+		} else {
+			$encoded_attrs = '';
+		}
+		return "<!-- wp:{$block_name} {$encoded_attrs}-->";
+	}
+
+	public static function block_closer( $block_name ) {
+		return "<!-- /wp:{$block_name} -->";
+	}
+
+	/**
+	 * Convert an array of WP_Block_Object objects to HTML markup.
+	 *
+	 * @param array $blocks The blocks to convert to markup.
+	 * @return string The HTML markup.
+	 */
+	public static function convert_blocks_to_markup( $blocks ) {
+		$block_markup = '';
+
+		foreach ( $blocks as $block ) {
+			// Allow mixing of inner blocks and content strings.
+			if ( is_string( $block ) ) {
+				$block_markup .= $block;
+				continue;
+			}
+			// Start of block comment
+			$block_markup .= self::block_opener( $block->block_name, $block->attrs );
+			$block_markup .= $block->attrs['content'] ?? '';
+			$block_markup .= self::convert_blocks_to_markup( $block->inner_blocks );
+			$block_markup .= self::block_closer( $block->block_name );
+		}
+
+		return $block_markup;
+	}
+
+	public static function slug_to_title( $filename ) {
+		$name = pathinfo( $filename, PATHINFO_FILENAME );
+		$name = preg_replace( '/^\d+/', '', $name );
+		$name = str_replace(
+			array( '-', '_' ),
+			' ',
+			$name
+		);
+		$name = ucwords( $name );
+		return $name;
+	}
+
+	public static function remove_first_h1_block_from_block_markup( $html ) {
+		$p = WP_Import_HTML_Processor::create_fragment( $html );
+		if ( false === $p->next_tag() ) {
+			return false;
+		}
+		if ( $p->get_tag() !== 'H1' ) {
+			return false;
+		}
+		$depth = $p->get_current_depth();
+		$title = '';
+		do {
+			if ( false === $p->next_token() ) {
+				break;
+			}
+			if ( $p->get_token_type() === '#text' ) {
+				$title .= $p->get_modifiable_text() . ' ';
+			}
+		} while ( $p->get_current_depth() > $depth );
+
+		if ( ! $title ) {
+			return false;
+		}
+
+		// Move past the closing comment
+		$p->next_token();
+		if ( $p->get_token_type() === '#text' ) {
+			$p->next_token();
+		}
+		if ( $p->get_token_type() !== '#comment' ) {
+			return false;
+		}
+
+		return array(
+			'content' => trim( $title ),
+			'remaining_html' => substr(
+				$html,
+				$p->get_string_index_after_current_token()
+			),
+		);
+	}
+}
