70691acadc611f1dc2acf972a6d8792614a6c2f0
Adam Zieliński <adam@adamziel.com>
2025-01-10 15:52:02 +0100

    Merge /WP_Filesystem_To_Post_Tree and /WP_Filesystem_Entity_Reader

--- packages/playground/data-liberation/bootstrap.php
+++ packages/playground/data-liberation/bootstrap.php
@@ -87,8 +87,9 @@ require_once __DIR__ . '/src/import/WP_Entity_Iterator_Chain.php';
 require_once __DIR__ . '/src/import/WP_Retry_Frontloading_Iterator.php';
 require_once __DIR__ . '/src/entity-readers/WP_Entity_Reader.php';
 require_once __DIR__ . '/src/entity-readers/WP_HTML_Entity_Reader.php';
-require_once __DIR__ . '/src/entity-readers/WP_Filesystem_To_Post_Tree.php';
 require_once __DIR__ . '/src/entity-readers/WP_Filesystem_Entity_Reader.php';
+
+require_once __DIR__ . '/src/WP_Data_Liberation_HTML_Processor.php';
 
 require_once __DIR__ . '/src/utf8_decoder.php';
 
